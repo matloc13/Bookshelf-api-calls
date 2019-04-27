b11dc06212f5021f68d043f74747081950a74de8
Matthew Lockdall <mlockdall26@gmail.com>
2019-04-27 17:28:25 -0500

    found the correct book with a query

--- js/app.js
+++ js/app.js
@@ -1,20 +1,36 @@
-const baseURL = `https://www.googleapis.com/books/v1/volumes?`
-const ipa = `key=AIzaSyAuDK04-7AA33VqCHIAXFQWieg8cPNBaKo`
-const baseTitle = `q=intitle:${queryTitle}`
-let queryTitle = 'The Grasshopper'
-let queryURL = baseURL + baseTitle + ipa
-
-console.log(queryURL);
+// https: //www.googleapis.com/books/v1/volumes?q=search+terms
+//   https: //books.google.com/ebooks?id=buc0AAAAMAAJ&dq=holmes&as_brr=4&source=webstore_bookcard
+//   GET https: //www.googleapis.com/books/v1/volumes/zyTCAlFPjgYC?key=yourAPIKey
 
 
-https: //www.googleapis.com/books/v1/volumes?q=search+terms
-  https: //books.google.com/ebooks?id=buc0AAAAMAAJ&dq=holmes&as_brr=4&source=webstore_bookcard
-  GET https: //www.googleapis.com/books/v1/volumes/zyTCAlFPjgYC?key=yourAPIKey
 
-  const findTitle = () => {
-    $.ajax({
-      url: queryURL,
-      type: "GET"
 
-    })
-  }
+const baseURL = `https://www.googleapis.com/books/v1/volumes?q=`;
+const ipa = `key=AIzaSyAuDK04-7AA33VqCHIAXFQWieg8cPNBaKo`;
+let mainSearch = `Foundation`
+let queryTitle = 'Foundation';
+let queryAuthor = 'Isaac+Asimov';
+const baseAuthor = `inauthor:${queryAuthor}`;
+const baseTitle = `intitle:${queryTitle}`;
+const bookType = `printType=books`
+
+let queryURL = baseURL + mainSearch + '+' + baseAuthor + '&' + ipa;
+
+// console.log(queryURL);
+
+
+
+
+const findTitle = () => {
+  $.ajax({
+    url: queryURL,
+    type: "GET"
+
+  }).then((data) => {
+    console.log(data);
+  })
+}
+
+$(() => {
+  findTitle()
+})
