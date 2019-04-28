6e832c114217cb51e55216726c29e017ca123e6f
Matthew Lockdall <mlockdall26@gmail.com>
2019-04-28 11:40:00 -0500

    search and advanced search are working if input is done correctly, more work on input js needed

--- js/app.js
+++ js/app.js
@@ -2,48 +2,58 @@
 //   https: //books.google.com/ebooks?id=buc0AAAAMAAJ&dq=holmes&as_brr=4&source=webstore_bookcard
 //   GET https: //www.googleapis.com/books/v1/volumes/zyTCAlFPjgYC?key=yourAPIKey
 
-const baseURL = `https://www.googleapis.com/books/v1/volumes?q=`;
-const ipa = `key=AIzaSyAuDK04-7AA33VqCHIAXFQWieg8cPNBaKo`;
-let mainSearch = `Harry+Potter`;
-let queryTitle = '';
-let queryAuthor = '';
-let querySubject = 'fantasy';
-const baseAuthor = `inauthor:${queryAuthor}`;
-const baseTitle = `intitle:${queryTitle}`;
-const baseSubject = `subject:${querySubject}`;
-const bookType = `printType=books`
-let queryURL = baseURL + mainSearch + '+' + baseAuthor + '&' + baseSubject + '&' + ipa;
+const searchObj = {
+  baseURL: `https://www.googleapis.com/books/v1/volumes?q=`,
+  ipa: `key=AIzaSyAuDK04-7AA33VqCHIAXFQWieg8cPNBaKo`,
+  mainSearch: '',
+  queryAuthor: '',
+  querySubject: ''
 
-console.log(queryURL);
+}
+// let baseAuthor = `inauthor:${searchObj.queryAuthor}`;
+// let baseTitle = `intitle:${searchObj.queryTitle}`;
+// let baseSubject = `subject:${searchObj.querySubject}`;
+let queryURL = searchObj.baseURL + searchObj.mainSearch + '+' + `inauthor:${searchObj.queryAuthor}` + '&' + `subject:${searchObj.querySubject}` + '&' + searchObj.ipa;
+
+
+
+
+// console.log(queryURL);
 
 const findTitle = () => {
   $.ajax({
-    url: queryURL,
-    // type: "GET"
-
+    url: queryURL
+    // type: "GET",
   }).then((data) => {
-    console.log(data);
-
+    // console.log(data);
     for (let i = 0; i < 10; i++) {
-      const book = $('<div>').addClass('book')
-      const newAuthor = $('<div>').text(data.items[i].volumeInfo.title)
+      const newAuthor = $('<div>').text(data.items[i].volumeInfo.title);
       const bookCover = $('<img>').attr('src', data.items[i].volumeInfo.imageLinks.thumbnail).on('click', () => {
         //display .description in a modal which also shows thumbnail
+        console.log(data);
       })
-      book.append(bookCover).append(newAuthor)
-      $('.resultsContainer').append(book)
+      const book = $('<div>').addClass('book').append(bookCover).append(newAuthor);
+      $('.resultsContainer').append(book);
     }
-    // $('.resultsContainer').html(`
-    //   <h3>${data.items[0].volumeInfo.title}</h3>
-    //   <h3>${data.items[0].volumeInfo.authors}</h3>`)
   })
 }
 
 $(() => {
 
   $('#submit').on('click', () => {
-    mainSearch = $('.inputMain').val();
-    findTitle();
+
+
+    // console.log($('.mainInput').val());
+    searchObj.mainSearch = $('.mainInput').val();
+    searchObj.queryAuthor = $('.author').val();
+    searchObj.querySubject = $('.genre').val();
+
+    queryURL = searchObj.baseURL + searchObj.mainSearch + '+' + `inauthor:${searchObj.queryAuthor}` + '&' + `subject:${searchObj.querySubject}` + '&' + searchObj.ipa;
+    // console.log(searchObj.mainSearch);
+    event.preventDefault()
+    // console.log(searchObj.mainSearch);
+    console.log(queryURL);
+    setTimeout(findTitle, 1000);
   })
-  findTitle();
+  // findTitle();
 })
