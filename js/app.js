f0410dd0cbfccc912fea57e772c649499082fcce
Matthew Lockdall <mlockdall26@gmail.com>
2019-04-30 19:55:27 -0500

    favorite added

--- js/app.js
+++ js/app.js
@@ -5,16 +5,18 @@ const searchObj = {
   queryAuthor: '',
   querySubject: ''
 }
+
 let queryURL = searchObj.baseURL + searchObj.mainSearch + '+' + `inauthor:${searchObj.queryAuthor}` + '&' + `subject:${searchObj.querySubject}` + '&' + searchObj.ipa;
 
 //ajax call and dom builder
 const findTitle = () => {
   $.ajax({
-    url: queryURL
+    url: queryURL,
+    maxResults: 5
     // type: "GET",
   }).then((data) => {
     // console.log(data);
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < data.items.length; i++) {
       const newAuthor = $('<div>').text(data.items[i].volumeInfo.title).css("border", "1px solid teal");
       const bookCover = $('<img>').attr('src', data.items[i].volumeInfo.imageLinks.thumbnail).on('click', (event) => {
         const modal = $('<div>').attr('id', 'modal')
@@ -29,22 +31,27 @@ const findTitle = () => {
         modalBox.appendTo(modal)
         modalBox.append(close)
         modal.appendTo('body');
-        //display .description in a modal which also shows thumbnail
+        //display .description in a modal which also
         // console.log(data);
       })
+      const favorite = $('<button>').addClass('cornerButton').addClass('favorite').on('click', (event) => {
+        $('.bookshelf').css('border', '1px solid teal');
+        $(event.target).parent().append($('<button>').addClass('cornerButton').text('remove').on('click', () => {
+          $(event.target).parent().remove();
+        }));
+
+        $(event.target).parent().appendTo($('.bookshelf'));
+        $(event.target).remove();
+      });
       const book = $('<div>').addClass('book').append(bookCover).append(newAuthor);
 
-      $('.newBooks').append(book);
+      $('.newBooks').append(book.append(favorite));
     }
   })
 }
 
-
-
-
 //jquery load wait
 $(() => {
-
 
   $('#submit').on('click', () => {
     let string = $('.mainInput').val(); //main input value
@@ -56,8 +63,14 @@ $(() => {
     queryURL = searchObj.baseURL + searchObj.mainSearch + '+' + `inauthor:${searchObj.queryAuthor}` + '&' + `subject:${searchObj.querySubject}` + '&' + searchObj.ipa;
     event.preventDefault();
     console.log(queryURL);
+    $('.newBooks').empty();
     setTimeout(findTitle, 1000);
+  });
+
+  $('.show').on('click', () => {
+    $('.advancedSearch').toggle();
   })
+
 
 
 
